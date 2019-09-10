2f50b1f20a63b93befb47665b3d71a0ebda72721
SSEGUSTAVO <54320247+SSEGUSTAVO@users.noreply.github.com>
2019-09-10 13:57:36 -0500

    Add files via upload

---
+++ Ejercicio 2/coperacha.java
@@ -0,0 +1,28 @@
+public class coperacha {
+    public static void main(String[] args) {
+        trucha caireles = new trucha();
+        caireles.debe = 10;
+        caireles.vaquita();
+        
+        caireles.debe = 12;
+        caireles.vaquita();
+        
+        caireles.debe = 9;
+        caireles.vaquita();
+        
+        caireles.debe = 8;
+        caireles.vaquita();
+        
+        caireles.debe = 7;
+        caireles.vaquita();
+        
+        caireles.debe = 15;
+        caireles.vaquita();
+        
+        caireles.debe = 11;
+        caireles.vaquita();
+        
+        caireles.debe = 30;
+        caireles.vaquita();
+    }
+}
---
+++ Ejercicio 2/trucha.java
@@ -0,0 +1,9 @@
+public class trucha {
+    public double impuesto = 0.05;
+    public double propina = 0.15;
+    public double debe = 10;
+    
+    public void vaquita(){
+        System.out.println("La persona debe: $ "+ (impuesto*debe+propina*debe+debe)); 
+    }
+}
